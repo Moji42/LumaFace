b5387b7ea1c694814c45e8afbb9edb731ee83417
Moji <najimoh170@gmail.com>
2025-05-18 12:56:34 -0700

    Set up Tailwind and started front end folder

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black-100 text-3xl font-bold">
+      Tailwind is working in LumaFace 
+    </main>
+  )
+}
+
